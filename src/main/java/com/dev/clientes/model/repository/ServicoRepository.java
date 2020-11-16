f68be082aca37659f8d106123ea2c63b89c7993e
HubertFer <humbertofernandezsosa@gmail.com>
2020-11-16 18:37:16 -0300

    Criacao entidades Cliente e Servico e testado persistencia em banco H2

---
+++ src/main/java/com/dev/clientes/model/repository/ServicoRepository.java
@@ -0,0 +1,7 @@
+package com.dev.clientes.model.repository;
+
+import com.dev.clientes.model.entity.Servico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServicoRepository extends JpaRepository<Servico, Integer> {
+}
